734c918ca4223e1a4f8e4f95eb36094016481b5e
Krishan Kumar Meghani <kkmeghan@gmail.com>
2020-08-11 15:53:13 +0530

    easy http

--- easyHttp/app.js
+++ easyHttp/app.js
@@ -1,13 +1,52 @@
-const easyhttp = new easyHttp();
-easyhttp.get('https://jsonplaceholder.typicode.com/posts',getposts);
+const http = new easyHTTP;
 
-function getposts(err,response){
+// Get Posts
+// http.get('https://jsonplaceholder.typicode.com/posts', function(err, posts) {
+//   if(err) {
+//     console.log(err);
+//   } else {
+//     console.log(posts);
+//   }
+// });
 
-  if(err === null)
-  {
+// Get Single Post
+// http.get('https://jsonplaceholder.typicode.com/posts/1', function(err, post) {
+//   if(err) {
+//     console.log(err);
+//   } else {
+//     console.log(post);
+//   }
+// });
+
+// Create Data
+const data = {
+  title: 'Custom Post',
+  body: 'This is a custom post'
+};
+
+// Create Post
+// http.post('https://jsonplaceholder.typicode.com/posts', data, function(err, post) {
+//   if(err) {
+//     console.log(err);
+//   } else {
+//     console.log(post);
+//   }
+// });
+
+// Update Post
+// http.put('https://jsonplaceholder.typicode.com/posts/5', data, function(err, post) {
+//   if(err) {
+//     console.log(err);
+//   } else {
+//     console.log(post);
+//   }
+// });
+
+// Delete Post
+http.delete('https://jsonplaceholder.typicode.com/posts/1', function(err, response) {
+  if(err) {
+    console.log(err);
+  } else {
     console.log(response);
   }
-  else{
-    console.log(err);
-  }
-}
+});
